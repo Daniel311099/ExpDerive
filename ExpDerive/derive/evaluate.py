0afb9bf5986ded69aa7fd3b50c67b8e42b3a34ef
Daniel311099 <danielfisaha@gmail.com>
2023-02-14 14:25:23 +0000

    first version of expression model

---
+++ ExpDerive/derive/evaluate.py
@@ -0,0 +1,7 @@
+from sympy import evaluate
+
+def t1(x):
+    return x + 1
+
+def t2(x):
+    return x + 2
---
+++ ExpDerive/derive/resolve.py
@@ -0,0 +1,215 @@
+from typing import Optional, TypedDict, List, Callable
+
+from sympy import Symbol, sympify, Function as SymFunction
+from sympy.parsing.latex import parse_latex
+from sympy.core.function import AppliedUndef
+# import Expression from sympy
+from sympy.core.expr import Expr
+
+class Variable(TypedDict):
+    custom: bool # leaf or not
+    plc: Optional[str]
+    column_name: str
+    table_name: str
+    latex: Optional[str]
+
+class ResolverReturn(TypedDict):
+    latex: str
+    variables: Optional[List[Variable]]
+
+class Func():
+    # TODO:
+    # defines a function object,
+    # a tree structure that is callable and is able to flatten itself out
+    # can be defined with a latex string or a pure python function
+    # wrap eval with a function that takes a string and returns the evaluated value
+    # refactor, move some functions from VAR to here 
+    pass
+
+class Var():
+    def __init__(self, name, leaf, latex=None, namespace=None):
+        self.name = name
+        self.latex = latex
+        self.expression: Optional[Expr] = parse_latex(latex) if latex else Symbol(name)
+        self.vars = {
+            # v: Var(v, '') 
+            # for v in parse_latex(latex).atoms(Symbol)
+        }
+        self.leaf = leaf
+        self.namespace = namespace
+
+    # def flatten(self, tree: str, resolver: Callable[[str, Optional[str]], ResolverReturn], variables: Optional[List[Variable]] = None):
+    #     explicit_vars = bool(variables)
+    #     exp = parse_latex(tree)
+    #     if explicit_vars:
+    #         variable_list = exp.atoms(Symbol)
+    #     else:
+    #         variable_list = [Symbol(v) for v in variables]
+    #     for v in variable_list:
+    #         if True:
+    #             # change to if v is a base column
+    #             # effectively the base case as each iteration will be skipped resulting in no recursive calls
+    #             continue
+    #         latex = resolver(v)
+    #         if explicit_vars:
+    #             flattened = self.flatten(latex, resolver, variables)
+    #         else:
+    #             flattened = self.flatten(latex, resolver)
+    #         exp = exp.subs(Symbol(v), flattened)
+        
+    #     return exp
+
+    def build(self, resolver, func_resolver): # recursive
+        # sub_funcs here
+        print(self.expression)
+        self.sub_funcs(func_resolver)
+
+        if self.leaf:
+            return
+
+        for v in self.expression.atoms(Symbol):
+            print(self.expression)
+            v_tree = resolver(v, self.namespace)
+            v_Var = Var(**v_tree)
+            v_Var.build(resolver, func_resolver)
+            self.vars[str(v)] = v_Var
+
+    def flatten(self):
+        if self.leaf:
+            return 
+        print(self.latex)
+        # print(self.vars)
+        for k, v in self.vars.items():
+            v.flatten()
+            print(self.expression, k, v)
+            self.expression = self.expression.subs(Symbol(k), v.expression)
+
+        print('//////////')
+
+    def unpack_func(self, exp, func_resolver):
+        # exp = parse_latex(latex)
+        funcs = exp.atoms(AppliedUndef)
+        print(funcs, 11, exp)
+
+        for f in funcs:
+            func = func_resolver(f.func)
+            args = f.args
+            definition = parse_latex(func['latex']) 
+            params = definition.atoms(Symbol)
+            for p, a in zip(params, args):
+                definition = definition.subs(p, a)
+            unpacked = self.unpack_func(definition, func_resolver)
+            exp = exp.subs(f, unpacked)
+        print(exp, 22)
+        return exp
+
+    def sub_funcs(self, func_resolver):
+        funcs = self.expression.atoms(AppliedUndef)
+        for f in funcs:
+            func = func_resolver(f.func)
+            # args = [Symbol(a) for a in func['args']]
+            args = f.args
+            definition = parse_latex(func['latex'])
+            definition = self.unpack_func(definition, func_resolver)
+            params = definition.atoms(Symbol)
+            print( list(zip(params, args)), f, definition)
+            for p, a in zip(params, args):
+                definition = definition.subs(p, a)
+            print(definition)
+            # self.expression = self.expression.subs(f, Function(func['latex'])(*args))
+            self.expression = self.expression.subs(f, definition)
+            # add support to unpack nested definitions, custom functions that call other custom functions
+            
+            
+
+class ExpTree():
+    def __init__(self, latex, name, exp_resolver=None, eval_resolver=None, namespace=None, func_resolver=None):
+        self.latex = latex
+        self.expression: Expr = parse_latex(latex)
+        self.exp_resolver = exp_resolver
+        self.eval_resolver = eval_resolver
+        self.variables: Optional[List] = None
+        self.flattened = None
+        self.namespace = namespace
+        self.tree = None
+        self.name = name
+        self.func_resolver = func_resolver
+        # TODO: add memoization
+    
+    def build_expression(self):
+        # TODO: add an args param to the resolver
+        tree = Var(self.name, False, self.latex, self.namespace)
+        
+        # tree.sub_funcs(self.func_resolver)
+
+
+        tree.build(self.exp_resolver, self.func_resolver)
+        tree.flatten()
+        # TODO: simplify tree.expression
+        # sub funcs
+        self.tree = tree
+        self.variables = tree.expression.atoms(Symbol)
+
+    # def flatten(self, expression: Expr):
+    #     # exp = parse_latex(self.latex)
+    #     vars = expression
+    #     if self.variables:
+    #         variable_list = self.variables
+    #     else:
+    #         variable_list = expression.atoms(Symbol)
+    #     for v in variable_list:
+    #         pass
+
+    def validate(self):
+        # TODO: 
+        # check arguments are valid
+        # check functions are valid
+        # check mathit is valid, use regex
+        pass
+    
+    def sub_funcs(self):
+        pass
+
+    def evaluate(self, subjects):
+        records = self.get_records(subjects)
+        evaluated = list(map(
+            lambda record: {
+                'subject': record['subject'],
+                'value': self.tree.expression.evalf(subs=record['stats'])
+            },
+            records
+        ))
+        return evaluated
+
+    def get_records(self, subjects):
+        records = [
+            {
+                'subject': subject,
+                'stats': {
+                    stat: self.eval_resolver(stat, subject)
+                    for stat in self.variables
+                }
+            }
+            for subject in subjects
+        ]
+        return records
+
+
+# # pass variables as a list if not encoded in the expression
+# # variables needs a resolver
+# # resolver is a function that takes the placeholder and returns the latex
+
+
+
+# def get_variables(exp):
+#     # add string manipulation
+
+#     return list(exp.atoms(Symbol))
+
+# # not exposed to the user
+# def sub_funcs(exp):
+#     for f in exp.atoms(AppliedUndef):
+#         if f.func not in resolver:
+#             raise Exception("Undefined function: {}".format(v.func))
+#         v.func = resolver[v.func]
+#     return exp
