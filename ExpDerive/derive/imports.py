95935b63ac3271dfe7e3e4fb541995083bc785b0
Daniel311099 <danielfisaha@gmail.com>
2023-02-23 14:54:56 +0000

    get around circular imports with common imports file

---
+++ ExpDerive/derive/imports.py
@@ -0,0 +1,12 @@
+# from .expression import Expression
+# from .evaluate import Evaluator
+# from .expression_components import Func, Var
+
+import ExpDerive.derive.expression as expression
+import ExpDerive.derive.evaluate as evaluate
+import ExpDerive.derive.expression_components as expression_components
+
+# Expression = expression.Expression
+# Evaluator = evaluate.Evaluator
+# Func = expression_components.Func
+# Var = expression_components.Var
